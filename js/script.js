3151785fe9c80a43e650640d639e168d97572c6d
EstherOkiri <est.okiri@gmail.com>
2024-03-17 11:18:01 +0300

    add dropdown logic

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ let navbar = document.querySelector( '.header .logo-section .navbar' );
 document.querySelector('#menuBtn').onclick = () =>{
     navbar.classList.toggle('.active');
 }
+let dropdownItems = document.querySelectorAll('.job-filter form .dropdown-container .dropdown .lists .items');
+
+dropdownItems.forEach(items =>{
+    items.addEventListener('click' = () =>{
+        let itemsParent = items.parentElement;
+        let output = itemsParent.querySelector('.output');
+        output.value = items.innerText;
+    });
+
+});
