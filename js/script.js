10e2df7038f4013dca12bec9b6c5b37a155fcf9b
EstherOkiri <est.okiri@gmail.com>
2024-03-25 20:10:19 +0300

    taking the value

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
-let navbar = document.querySelector( '.header .logo-section .navbar' );
-document.querySelector('#menuBtn').onclick = () =>{
-    navbar.classList.toggle('.active');
-}
-let dropdownItems = document.querySelectorAll('.job-filter form .dropdown-container .dropdown .lists .items');
+// let navbar = document.querySelector( '.header .logo-section .navbar' );
+// document.querySelector('#menuBtn').onclick = () =>{
+//     navbar.classList.toggle('.active');
+// }
+// let dropdownItems = document.querySelectorAll('.job-filter form .dropdown-container .dropdown .lists .items');
 
-dropdownItems.forEach(items =>{
-    items.addEventListener('click' = () =>{
-        let itemsParent = items.parentElement;
-        let output = itemsParent.querySelector('.output');
-        output.value = items.innerText;
-    });
+// dropdownItems.forEach(items =>{
+//     items.addEventListener('click' = () =>{
+//         let itemsParent = items.parentElement;
+//         let output = itemsParent.querySelector('.output');
+//         output.value = items.innerText;
+//     });
 
-});
+// });
+fetch('https://jobs-api14.p.rapidapi.com/list').then(res => res.json())
+.then(data =>{
+
+})
+
+//home page search
+const jobs = [];
+const searchInput = document.getElementById('searchinput')
+//add event listener
+searchInput.addEventListener('input',(e)=>{
+    const value = e.target.value
+    console.log(value)
+})
+//loop through jobs
+
