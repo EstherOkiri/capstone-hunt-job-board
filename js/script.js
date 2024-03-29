6f98b10db56fa4d745bdb5aedfcb68ebdddbcc53
EstherOkiri <est.okiri@gmail.com>
2024-03-29 21:02:28 +0300

    log in with google update

--- js/script.js
+++ js/script.js
@@ -1,29 +1,13 @@
-// let navbar = document.querySelector( '.header .logo-section .navbar' );
-// document.querySelector('#menuBtn').onclick = () =>{
-//     navbar.classList.toggle('.active');
-// }
-// let dropdownItems = document.querySelectorAll('.job-filter form .dropdown-container .dropdown .lists .items');
+const searchInput = document.getElementById('searchinput');
+const searchInputValue = searchInput.value;
+const searchJobBtn = document.getElementById('search-job-button')
 
-// dropdownItems.forEach(items =>{
-//     items.addEventListener('click' = () =>{
-//         let itemsParent = items.parentElement;
-//         let output = itemsParent.querySelector('.output');
-//         output.value = items.innerText;
-//     });
+console.log(searchInput);
 
-// });
-fetch('https://jobs-api14.p.rapidapi.com/list').then(res => res.json())
-.then(data =>{
-
-})
-
-//home page search
-const jobs = [];
-const searchInput = document.getElementById('searchinput')
-//add event listener
-searchInput.addEventListener('input',(e)=>{
-    const value = e.target.value
-    console.log(value)
-})
-//loop through jobs
-
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
