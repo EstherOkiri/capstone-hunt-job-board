39e48a47ecc155ae810e41331099c7c0748132f3
EstherOkiri <est.okiri@gmail.com>
2024-04-12 09:03:58 +0300

    updated links

--- js/script.js
+++ js/script.js
@@ -38,27 +38,26 @@ var jobs = [
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   }
 ]
-const params = new URLSearchParams(window.location.search);
-const titleSearch = params.get('title');
-const filterSearchInput = params.get('filter-search-input')
+        const params = new URLSearchParams(window.location.search);
+        const title = params.get('title');
 
-   // Filter jobs based on title
-const filteredJobs = jobs.filter(job =>
+ 
+        // Filter jobs based on keywords
+        const filteredJobs = jobs.filter(job => 
+            job.jobTitle.toLowerCase().includes(title.toLowerCase())
+        );
 
-    job.jobTitle.toLowerCase().includes(titleSearch.toLowerCase()) ||
-    job.type.toLowerCase().includes(titleSearch.toLowerCase()) ||
-    job.location.toLowerCase().includes(titleSearch.toLowerCase())
-
-)
-// Display filtered jobs
-const resultsContainer = document.getElementById('results-container');
+        // Display filtered jobs
+        const resultsContainer = document.getElementById('results-container');
         filteredJobs.forEach(job => {
             const jobElement = document.createElement('div');
             jobElement.innerHTML = `
-                <h1>${job.jobTitle}</h1>
-                <h2><strong>Location:</strong> ${job.location}</h2>
-                <h3>${job.type}</h3>
+                <h3>${job.jobTitle}</h3>
+                <p><strong>Location:</strong> ${job.location}</p>
+                <p><strong>Location:</strong> ${job.country}</p>
+                <p>${job.type}</p>
                 <p>${job.description}</p>
+                <p><strong>Location:</strong> ${job.salary}</p>
             `;
             resultsContainer.appendChild(jobElement);
         });
